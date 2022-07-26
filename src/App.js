17103c3f6b4f5865c20631e11338f7c109d40e5d
zeredogo <zeredogo@gmail.com>
2022-07-26 21:42:17 +0100

    updating commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+  return (
+    <ChartEngine 
+       height="100vh" 
+       projectID=""
+       userName=""
+       userSecret=""
+    />
+  )
+}
+
+export default App
